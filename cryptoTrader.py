77aa3b2a8cebe37608f5618dfb2d3446a21a92ab
Richard <Richard.felius@gmail.com>
2021-10-21 22:46:27 +0200

    updated cryptobot

--- cryptoTrader.py
+++ cryptoTrader.py
@@ -10,25 +10,16 @@ from NeuralNet.neuralNet import Population
 
 symbol = 'BTCEUR'
 PLAYER_AMOUNT = 70
-switch_fitness = 150 
-
-markets = os.listdir('Data_bitcoin/')
-key = random.choice(markets)
-df = pd.read_csv(f'Data_bitcoin//{key}', index_col=0).fillna(0)
-metrics = df.loc[:,'close_value':].drop(columns = 'symbol')
-norm_metrics = ((metrics - metrics.mean()) / metrics.std())
-
-#%%
 
 class TradingBot:
-    def __init__(self, metrics, verbose, logging):
+    def __init__(self, verbose, logging):
         self.trading_fee = 0.0001
-        self.data = metrics
+        self.select_market()
         self.generation = 0
         self.population = Population(PLAYER_AMOUNT)
-        self.input_nodes = len(metrics.columns)
+        self.input_nodes = len(self.data.columns)
         self.output_nodes = 1
-        self.init_connections = 5
+        self.init_connections = 15
         self.output_node_number = self.input_nodes + self.output_nodes - 1
         self.population.create_population(self.input_nodes,self.output_nodes, self.init_connections)
         self.innovation_df = pd.DataFrame(columns = ['Abbrev', 'Innovation_number'])
@@ -38,18 +29,31 @@ class TradingBot:
         self.logging = logging
         self.log = pd.DataFrame()
 
+    def select_market(self):
+        markets = os.listdir('Data_bitcoin/')
+        key = random.choice(markets)
+        print(f'Now using market {key} for caluclation')
+        df = pd.read_csv(f'Data_bitcoin//{key}', index_col=0).fillna(0)
+        metrics = df.loc[1000:,'close_value':].drop(columns = 'symbol')
+        norm_metrics = ((metrics - metrics.mean()) / metrics.std())
+        size_dataset = 10000
+        first_sample = np.random.randint(0,len(norm_metrics)-size_dataset)
+        self.data = norm_metrics[first_sample:first_sample+size_dataset]
+
     def trade(self):
         while self.population.generation < 100:
-            self.calculate()
+            network_Score = self.calculate()
             self.reproduce()
-        
+            if np.mean(network_Score[:4] ) > 1500: 
+                self.select_market()
+
     def calculate(self):
         network_Score = []
         for _, network in enumerate(self.population):
             bank_account = 1000
             coins_wallet = 0
             trades = 0
-            for _, row in metrics.loc[1000:,:].iterrows():
+            for _, row in self.data.iterrows():
                 network.run_live(row)
                 for output_node in range(self.output_nodes):
                     output_node_index = self.input_nodes + output_node
@@ -68,7 +72,7 @@ class TradingBot:
                         coins_wallet = 0
                         trades += 1
             
-            change =  metrics.tail(1)['close_value'].values[0] / metrics.loc[1000,'close_value'] 
+            change =  self.data.tail(1)['close_value'].values[0] / self.data.loc[1000,'close_value'] 
             network.fitness = int((bank_account + coins_wallet * row['close_value']) / change)
             network.trades = trades
             if self.verbose:
@@ -76,7 +80,8 @@ class TradingBot:
             network_Score.append(network.fitness)
         self.max_fitness = max(network_Score)
         print(f'Maximal fitness of generation: {self.max_fitness}')
-            
+        return network_Score
+
     def reproduce(self):
         if self.logging:
             self.log = self.log.append(self.population.log())
@@ -86,6 +91,9 @@ class TradingBot:
 
 
 # %%
-tradeBot = TradingBot(metrics, verbose = False, logging = False)
+tradeBot = TradingBot(verbose = False, logging = False)
 tradeBot.trade()
 
+
+
+# %%
